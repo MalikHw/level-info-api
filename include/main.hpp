f18c77f4a3292d2e0afd8dc13b36196d6912ee04
MalikHw47 <123307564+MalikHw@users.noreply.github.com>
2026-05-12 09:50:14 +0100

    Add LevelData structure and LevelFetchAPI namespace

---
+++ include/main.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include <Geode/Geode.hpp>
+
+using namespace geode::prelude;
+
+struct LevelData {
+    int levelID;
+    std::string levelName;
+    std::string creator;
+    std::string description;
+    int length; // 0=tiny, 1=short, 2=medium, 3=long, 4=xl, 5=plat
+    int likes; // negative = dislikes
+    int downloads;
+    std::string songName;
+    bool featured;
+    bool epic;
+};
+
+using LevelDataCallback = std::function<void(std::optional<LevelData>)>;
+
+namespace LevelFetchAPI {
+    void fetchLevel(int levelID, LevelDataCallback callback);
+}
